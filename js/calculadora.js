cb978f5bdb4489fc5331e19b6b5f5c041a661b0a
Guido Fava <guidofava1933@gmail.com>
2022-08-11 20:28:48 -0300

    version2

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,9 +2,9 @@ function calcular() {
     let primerNro = parseInt(prompt("Ingresa el monto en dolares"))
     let segundoNro = parseInt(prompt("Ingresa la cotizacion del dolar oficial"))
     console.log("Monto en pesos con impuestos", realizarCalculo(primerNro, segundoNro))
+
 }
 
 function realizarCalculo(num1, num2) {
-    return (num1 * num2) * 1.75
-
+    alert((num1 * num2) * 1.75)
 }
